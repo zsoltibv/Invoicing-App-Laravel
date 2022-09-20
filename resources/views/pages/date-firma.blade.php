2b065376814d89fb876a8922d7487d93318f14be
zsoltibv <zsoltibv@gmail.com>
2022-09-20 20:16:44 +0300

    update

--- resources/views/pages/date-firma.blade.php
+++ resources/views/pages/date-firma.blade.php
@@ -40,8 +40,13 @@
                 </div>
                 <div class="controls">
                     <label for="cod_fiscal" class="col-md-4 col-form-label">{{ __('Cod Fiscal*') }}</label>
-                    <div class="py-3 flex flex-col">
-                        <input id="cod_fiscal" type="text" class="h-9 p-2 text-sm bg-blue-100
+                    <div class="py-3 flex items-center">
+                        {{-- form to autocomplete --}}
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="35" height="35" class="p-1 bg-sky-800 fill-white"><path d="M23.5 22H23v-2h.5a4.5 4.5 0 0 0 .36-9H23l-.1-.82a7 7 0 0 0-13.88 0L9 11h-.86a4.5 4.5 0 0 0 .36 9H9v2h-.5A6.5 6.5 0 0 1 7.2 9.14a9 9 0 0 1 17.6 0A6.5 6.5 0 0 1 23.5 22Z"/><path d="M17 26.17V14h-2v12.17l-2.59-2.58L11 25l5 5 5-5-1.41-1.41L17 26.17z"/><path fill="none" d="M0 0h32v32H0z" data-name="&lt;Transparent Rectangle&gt;"/></svg>
+                        </button>
+                        {{-- end --}}
+                        <input id="cod_fiscal" type="text" class="h-9 p-2 text-sm bg-blue-100 w-full
                                  @error('cod_fiscal') is-invalid @enderror" name="cod_fiscal" value="" required
                             autocomplete="cod_fiscal" autofocus>
 

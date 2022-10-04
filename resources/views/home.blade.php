74fed9ed8d32254cd3a25e0ae43376647f0163e2
zsoltibv <zsoltibv@gmail.com>
2022-10-04 16:35:17 +0300

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,11 @@
     <div class="mx-auto container py-3 my-6 px-3">
         <div class="hero flex lg:items-center font-body" style="height: 65vh;">
             <div class="col-1">
-                <h3 class="text-sm md:text-base text-blue-700 mb-3 font-medium">MEET FACTURILA</h3>
+                <h3 class="text-sm md:text-base text-blue-700 mb-3 font-medium">MEET FACTURIL</h3>
                 <h1 class="text-2xl 2-xl:text-5xl lg:text-4xl md:text-3xl font-semibold md:pb-3">Simplifică emiterea facturilor.</h1>
                 <h1 class="text-2xl 2-xl:text-5xl lg:text-4xl md:text-3xl font-semibold pb-14 md:pb-20">Amplifică încasările.</h1>
                 <a href="{{route('register')}}" class="bg-black px-8 py-2.5 md:px-10 md:py-3.5 text-white text-sm">
-                        Începe Acu
+                        Începe Acum
                 </a>
             </div>
         </div>
